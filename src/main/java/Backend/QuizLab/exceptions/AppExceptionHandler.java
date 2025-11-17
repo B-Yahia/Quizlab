44c8d2354e68a6ebe7ecce250a83eb2932b0fa46
B-Yahia <yahiabahhous@gmail.com>
2025-11-17 16:35:44 +0200

    feat: implement comprehensive exception handling
    Add global exception handler with logging and common exception types support

--- src/main/java/Backend/QuizLab/exceptions/AppExceptionHandler.java
+++ src/main/java/Backend/QuizLab/exceptions/AppExceptionHandler.java
@@ -1,10 +1,14 @@
 package Backend.QuizLab.exceptions;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +19,7 @@ public class AppExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public Map<String,String> handleNotFoundEx (ResourceNotFoundException exception){
         Map<String, String> errorMsg = new HashMap<>();
-        errorMsg.put("Messages", exception.getMessage());
+        errorMsg.put("message", exception.getMessage());
         return  errorMsg;
     }
 
@@ -27,4 +31,46 @@ public class AppExceptionHandler {
                 errors.put(error.getField(), error.getDefaultMessage()));
         return errors;
     }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(DataAccessException.class)
+    public Map<String, String> handleDataAccessException(DataAccessException exception) {
+        Map<String, String> errorMsg = new HashMap<>();
+        errorMsg.put("message", exception.getMessage());
+        return errorMsg;
+    }
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public Map<String, String> handleMethodNotSupported(HttpRequestMethodNotSupportedException exception) {
+        Map<String, String> errorMsg = new HashMap<>();
+        errorMsg.put("message", "HTTP method not supported for this endpoint");
+        return errorMsg;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public Map<String, String> handleMessageNotReadable(HttpMessageNotReadableException exception) {
+        Map<String, String> errorMsg = new HashMap<>();
+        errorMsg.put("message", "Invalid JSON format in request body");
+        return errorMsg;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public Map<String, String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException exception) {
+        Map<String, String> errorMsg = new HashMap<>();
+        errorMsg.put("message", "Invalid parameter type for: " + exception.getName());
+        return errorMsg;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public Map<String, String> handleAllExceptions(Exception exception) {
+        Map<String, String> errorMsg = new HashMap<>();
+         errorMsg.put("message", exception.getMessage());
+         errorMsg.put("error", exception.getClass().getSimpleName());
+        return errorMsg;
+    }
+
 }
